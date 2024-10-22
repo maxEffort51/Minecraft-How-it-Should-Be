76f0a0f0a0455d46a65368e55d0b0dfb853ac46b
maxEffort51 <aaron.terry09@icloud.com>
2024-10-21 20:00:43 -0500

    Initial commit

---
+++ src/main/java/net/aaronterry/hisb/HisbMod.java
@@ -0,0 +1,17 @@
+package net.aaronterry.hisb;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HisbMod implements ModInitializer {
+	public static final String MOD_ID = "hisb";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/aaronterry/hisb/HisbModClient.java
@@ -0,0 +1,10 @@
+package net.aaronterry.hisb;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HisbModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
